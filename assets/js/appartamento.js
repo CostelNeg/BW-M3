976e1f1e5645b4261140714e3b33c937103e8f79
mirko-mc <8547740+mirko-mc@users.noreply.github.com>
2024-05-18 05:15:40 +0200

    Mirko (#1)
    * basic structure of the gallery and description
    
    * card structure
    
    * structure complete except margin, padding
    
    * update aside and dove dormirai
    
    * completato card laterale e dove dormirai. iniziato cosa troverai
    
    * my page section completed

---
+++ assets/js/appartamento.js
@@ -0,0 +1,19 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const calendarEl = document.getElementsByClassName("calendario");
+  for (const cal of calendarEl) {
+    const calendar = new FullCalendar.Calendar(cal, {
+      initialView: "dayGridMonth",
+    });
+    calendar.render();
+  }
+});
+
+function toggleDescrizione() {
+  document.getElementById("descrizione").classList.toggle("descrizione");
+}
+
+function toggleCarousel() {
+  const main = document.getElementsByTagName("main");
+  main[0].classList.toggle("d-none");
+  document.getElementById("carosello").classList.toggle("d-none");
+}
